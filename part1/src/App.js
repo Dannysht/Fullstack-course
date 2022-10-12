32ef9052797bb172b115b446d1ebd1f6ed9ebb39
Daniel <dannysht27@gmail.com>
2022-10-12 19:37:08 +0200

    Finished task 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,4 @@
-//Exercise 1.1
+//Exercise 1.2
 
 const App = () =>  
 {
@@ -32,14 +32,19 @@ const Content = (props) =>
 {
   return(
     <div>
+      <Part part = {props.part1} exercises = {props.exercises1}/>
+      <Part part = {props.part2} exercises = {props.exercises2}/>
+      <Part part = {props.part3} exercises = {props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) =>
+{
+  return(
+    <div>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </div>
   )
