0ec73d2f34c5ebd65c59404ebe2fc15567395967
Daniel <dannysht27@gmail.com>
2022-10-12 21:25:30 +0200

    completed task 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,6 +2,15 @@
 
 import { useState } from "react";
 
+const StatisticLine = (props) =>
+{
+  return(
+    <div>
+      <p>{props.text} {props.value}</p>
+    </div>
+  )
+}
+
 const Statistics = (props) =>
 {
   if(props.all === 0)
@@ -11,12 +20,13 @@ const Statistics = (props) =>
   return(
     <div>
       <h1>statistics</h1>
-        <p>good {props.good}</p>
-        <p>neutral {props.neutral}</p>
-        <p>bad {props.bad}</p>
-        <p>all {props.all}</p>
-        <p>average {(props.good-props.bad)/props.all}</p>
-        <p>positive {100*props.good/props.all}%</p>
+        <StatisticLine text= "good" value={props.good}/>
+        <StatisticLine text= "neutral" value={props.good}/>
+        <StatisticLine text= "bad" value={props.good}/>
+        <StatisticLine text= "all" value={props.good}/>
+        <StatisticLine text= "average" value={props.average}/>
+        <StatisticLine text= "positive" value={props.positive}/>
+      
     </div>
   )
 }
@@ -27,14 +37,15 @@ const App = () =>
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const all = good+bad+neutral
-
+  const average = (good-bad)/all
+  const positive = (100*good/all)+"%"
   return(
     <div>
       <h1>give feedback</h1>
       <button onClick={()=>setGood(good+1)}>good</button>
       <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
       <button onClick={()=>setBad(bad+1)}>bad</button>
-      <Statistics good={good} bad={bad} neutral={neutral} all = {all}/>
+      <Statistics good={good} bad={bad} neutral={neutral} all = {all} average ={average} positive = {positive}/>
     </div>
   )
 }
