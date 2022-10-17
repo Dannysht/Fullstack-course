9f23625c2a330bb30d4a597d23ef63fd54c042bb
Daniel <dannysht27@gmail.com>
2022-10-17 19:37:14 +0200

    finished exercise 2.4

--- part2/Exercise 2.1/src/App.js
+++ part2/Exercise 2.1/src/App.js
@@ -1,34 +1,55 @@
-//Exercise 2.3
+//Exercise 2.4
 
 const App = () =>  
 { 
-  const course = {
+  const course = [{
     name: 'Half Stack application development',
+    id: 1,
     parts:
     [  
       {
         name : "Fundamentals of React",
+        id: 1,
         exercises : 10
       },
       {
         name: "Using props to pass data",
+        id: 2,
         exercises: 7
       },
       {
         name: "State of a component",
+        id: 3,
         exercises: 14
       },
       {
         name: "Redux",
+        id: 4,
         exercises: 11
       }
     ]
-  }
+  },
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }]
+    }
+]
 
 
   return (
   <div>
-    <Course course = {course}/>
+    {course.map(course => <Course key = {course.id} course={course}/>)}
   </div>
   )
 }
@@ -55,12 +76,11 @@ const Header = (props) =>
 
 const Content = (props) => 
 {
+  console.log(props.course);
   return(
-    <div>
-      <Part part = {props.course.parts[0].name} exercises = {props.course.parts[0].exercises}/>
-      <Part part = {props.course.parts[1].name} exercises = {props.course.parts[1].exercises}/>
-      <Part part = {props.course.parts[2].name} exercises = {props.course.parts[2].exercises}/>
-    </div>
+  <div>
+    {props.course.parts.map(course => <Part key = {course.id} part = {course.name} exercises = {course.exercises}/>)}
+  </div>
   )
 }
 
@@ -77,9 +97,7 @@ const Part = (props) =>
 
 const Footer = (props) =>
 {
-  //let total = 0
   let totalExercises = props.course.parts.reduce((first, second) => first + second.exercises, 0)
-  //props.course.parts.map(parts => {totalExercises +=parts.exercises;return true})
   return(
     <div>
       <p>
